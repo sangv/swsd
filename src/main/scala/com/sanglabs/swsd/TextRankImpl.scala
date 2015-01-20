f653b28af057b7c86135b1047926fc0d11d907f6
sangv <sang.venkatraman@museaic.com>
2015-01-19 22:13:56 -0500

    added simple text rank implementation using jung graph

---
+++ src/main/scala/com/sanglabs/swsd/TextRankImpl.scala
@@ -0,0 +1,68 @@
+package com.sanglabs.swsd
+
+import edu.uci.ics.jung.algorithms.scoring.PageRank
+import edu.uci.ics.jung.graph.{DirectedSparseGraph, Graph}
+import net.sf.extjwnl.data.POS
+
+import scala.collection.immutable.ListMap
+
+/**
+ *
+ * The TextRankImpl
+ *
+ * @author Sang Venkatraman
+ *
+ */
+object TextRankImpl {
+
+  def calculate(text: String): ListMap[WordAnalysis,Double] =
+  {
+
+    val graph: Graph[WordAnalysis, String] = new DirectedSparseGraph[WordAnalysis, String]();
+
+    var outerEdgeMap = Map[WordAnalysis, WordAnalysis]()
+
+    var edgeIndex = -1
+
+    //no compoundification considered and filterd by POS
+    val sentences = StanfordNLPService.analyze(text)
+
+    for (sentence <- sentences) {
+
+      val words = sentence.words filter (p => (POS.NOUN.equals(p.pos) || POS.ADJECTIVE.equals(p.pos)))
+
+
+      //window size 2
+      for (List(first, second, third) <- words.sliding(3)) {
+        graph.addVertex(first)
+        graph.addVertex(second)
+        graph.addVertex(third)
+
+        //We are ignoring multiple same edges between same nodes
+        edgeIndex += 1
+        outerEdgeMap += (first -> second)
+        graph.addEdge("links_to_" + edgeIndex, first, second)
+
+        //if(graph.findEdge(first,third) != null)
+        edgeIndex += 1
+        graph.addEdge("links_to_" + edgeIndex, first, third)
+        outerEdgeMap += (first -> third)
+      }
+
+    }
+
+    val ranker = new PageRank(graph, 0.15);
+    ranker.evaluate();
+
+    var scores = Map[WordAnalysis, Double]()
+    for (word <- outerEdgeMap.keys) {
+      scores += (word -> ranker.getVertexScore(word))
+      //println(s"${word.word} + ${word.pos} => ${ranker.getVertexScore(word)}")
+    }
+
+    val result = ListMap(scores.toList.sortBy(_._2).reverse: _*)
+    result  foreach (p => println(s"${p._1} => ${p._2}"))
+    result
+  }
+
+}
---
+++ src/test/scala/com/sanglabs/swsd/TextRankImplTest.scala
@@ -0,0 +1,30 @@
+package com.sanglabs.swsd
+
+import org.junit.Assert._
+import org.junit.Test
+import org.scalatest.junit.{JUnitSuite, ShouldMatchersForJUnit}
+
+import scala.collection.immutable.ListMap
+
+/**
+ *
+ * The TextRankImplText 
+ *
+ * @author Sang Venkatraman
+ *
+ */
+class TextRankImplTest extends JUnitSuite with ShouldMatchersForJUnit {
+
+
+  @Test
+  def test1() {
+    val result: ListMap[WordAnalysis,Double] = TextRankImpl.calculate("Cassandra is a NoSQL database that provides extremely fast writes (and reads with the correct design). It also has availability and partitioning built into its design with tunable consistency. To achieve distribution as a fundamental construct, Cassandra recommends and imposes certain querying limitations. Cassandra like other NoSQL databases also encourages denormalization (allowing data duplication) and enforces a schema design that is in keeping with how the data would be queried.\n\nWhen building a system of record, it is not always possible to foresee all kinds of queries that need to be executed on the data. But, because we want to leverage all the built in distribution and scaling capabilities of Cassandra, it makes sense for the system of record data to live in Cassandra (especially as the volume of data is high or bound to increase).")
+
+    val topWords = result.take(3).map(_._1.word).toList
+    assertEquals("data", topWords(0))   //0.10759560207132277
+    assertEquals("design", topWords(1))  //0.07028082988453872
+    assertEquals("Cassandra", topWords(2)) // 0.06832425956935244
+
+  }
+
+}
